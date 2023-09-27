6591cad6f0fb4d11d27aba3d149047435de5d958
Felipe Vogt <felipevogt.f@gmail.com>
2023-09-27 13:37:13 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { Drivers } from '@ionic/storage';
 import { AuthInterceptor } from './interceptos/auth.interceptor';
-import { LightgalleryModule } from 'lightgallery/angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,7 +25,6 @@ import { LightgalleryModule } from 'lightgallery/angular';
     }),
     AppRoutingModule,
     HttpClientModule,
-    LightgalleryModule
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
